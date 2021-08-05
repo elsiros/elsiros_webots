bcf8d579ca5919f1053d3e995b55905510576f38
HiTF <ryakin.is@phystech.edu>
2021-08-05 19:17:35 +0300

    test position send

--- controllers/player/client.py
+++ controllers/player/client.py
@@ -3,13 +3,13 @@ import messages_pb2
 
 import socket
 import sys
-
+import time
+from robot_client import RobotClient
 
 def usage(error_msg = ""):
     if (error_msg):
         print("Invalid call: %s\n", error_msg)
     print("Usage: client [-v verbosity_level] <host> <port>\n")
-
 
 def run():
     port = -1
@@ -35,32 +35,53 @@ def run():
             else:
                 usage("Unexpected additional argument: " + current_arg)
         arg_idx+=1
+    
   
     if (port == -1):
         usage("Missing arguments")
 
-    #host = '127.0.0.1'
-    #port = 10001
-    
-    
-    robot_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    robot_client.connect((host, port))
-    
+    host = '127.0.0.1'
+    port = 10001
+    verbosity = 4
+    robot_client = RobotClient(host, port, verbosity)
+    robot_client.connect_client()
+    #if(True):
     while(True):
-        
-        try:
+        #try:
+        actuator_request = robot_client.build_request_message("actuator_requests.txt")
+        actuator_request = messages_pb2.ActuatorRequests()
+        motor = actuator_request.motor_positions.add()
+        motor.name = "head_yaw"
+        motor.position = 1.7
+      
+      
+        robot_client.send_request(actuator_request)
+        time.sleep(0.1)
+        #robot_client.receive()
+        #except Exception as ex:
+        #    print("Runtime error: " + str(ex))
+    
+    
+    
+    
+    return 0
+    while(True):
+        if (True):
+        #try:
             request = messages_pb2.ActuatorRequests()
             with open("actuator_requests.txt") as actuator_requests:
                 text_format.Parse(actuator_requests.read(), request)
             
             robot_client.send(request.SerializeToString())
-            data = robot_client.recv(1024)
-            sensors = messages_pb2.SensorMeasurements()
-            ans = sensors.ParseFromString(data)
             
-            print(ans)
-        except Exception as ex:
-            print("Runtime error: " + str(ex))
+            data = robot_client.recv(1024)
+            print(data)
+            sensors = messages_pb2.SensorMeasurements()
+            sensors.ParseFromString(data)
+            
+            print(sensors.cameras)
+        ##except Exception as ex:
+        #    print("Runtime error: " + str(ex))
 
 if __name__ == '__main__':
     
--- controllers/player/robot_client.py
+++ controllers/player/robot_client.py
@@ -1,26 +1,144 @@
 import socket
+import sys
+import time
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 10001        # Port to listen on (non-privileged ports are > 1023)
+from google.protobuf.internal.decoder import _DecodeVarint32
+from google.protobuf.message import DecodeError
+from google.protobuf import text_format
+import messages_pb2
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as s:
-    s.connect((HOST, PORT))
-    # s.bind((HOST, PORT))
-    welcome_message = s.recv(8)
+max_attempts = 20
+wait_time_sec = 1
 
-    if welcome_message == b'Welcome\x00':
-        print("I'm connected")
-    elif welcome_message == b'Refused\x00':
-        print("Connection refused")
+
+def usage(error_msg=""):
+    if (error_msg):
+        print("Invalid call: %s\n", error_msg)
+    print("Usage: client [-v verbosity_level] <host> <port>\n")
+
+
+class RobotClient():
+    def __init__(self, host='',  port=-1, verbosity=3):
+        self.host = host
+        self.port = port
+        self.verbosity = verbosity
+
+    def connect_client(self):
+        try:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except:
+            if (self.verbosity > 0):
+                print("Cannot create socket\n")
+                return False
+        attempt = 1
+        connected = False
+        for attempt in range(max_attempts):
+            try:
+                self.socket.connect((self.host, self.port))
+                connected = True
+                break
+            except:
+                print("Failed to connect to ", self.host, " : ",
+                      self.port, " attempt ",  attempt, " of ", max_attempts)
+                time.sleep(wait_time_sec)
+
+        if not connected:
+            if (self.verbosity > 0):
+                print("Failed to connect after ", attempt,
+                      "attempts. Giving up on connection")
+            self.disconnect_client()
+            return False
+
+        # Receiving the 'welcome message'
+        welcome_message = self.socket.recv(8)
+        if (self.verbosity >= 4):
+            print("Welcome message:", welcome_message.decode("utf-8"))
+        if welcome_message != b'Welcome\x00':
+            if (self.verbosity > 0):
+                if welcome_message == b'Refused\x00':
+                    print("Connection refused")
+                else:
+                    print("Received unknown answer from server: ",
+                          welcome_message.decode("utf-8"))
+            self.disconnect_client()
+            return False
+
+        if (self.verbosity >= 2):
+            print("Connected to ", str(self.host) + " :", self.port)
+        return True
+
+    def printMessages():
+        pass
+
+    def disconnect_client(self):
+        self.socket.close()
+
+    def send_request(self, actuator_request):
+        #try:
+        self.socket.send(actuator_request.SerializeToString())
+        print("send servos")
+        #except:
+        #    print("Can't send request")
+
+    def build_request_message(self, path):
+        request = messages_pb2.ActuatorRequests()
+        with open(path) as actuator_requests:
+            text_format.Parse(actuator_requests.read(), request)
+        return request
+
+    def receive(self):
+        sensors_measurements = messages_pb2.SensorMeasurements()
+        
+        data = self.socket.recv(1024)
+        #print(data)
+
+        data = self.socket.recv(1024)# read file as string
+        decoder = _DecodeVarint32          # get a varint32 decoder
+                                         # others are available in varint.py
+
+        next_pos, pos = 0, 0
+        while pos < len(data):
+            sensors_measurements = messages_pb2.SensorMeasurements()                    # your message type
+            next_pos, pos = decoder(data, pos)
+            sensors_measurements.ParseFromString(data[pos:pos + next_pos])
+            #print(sensors_measurements.objects)
+            pos += next_pos     
+    # use parsed message
+
     
-    
-    # s.listen()
-    # conn, addr = s.accept()
+        #data = ''.join(self.socket.recv_multipart())
+        #time.sleep(0.1)
+        #sensors_measurements.ParseFromString(data)
+        
+        #print(sensors_measurements.gps)
 
-    # with conn:
-    #     print('Connected by', addr)
-    #     while True:
-    #         data = conn.recv(1024)
-    #         if not data:
-    #             break
-    #         conn.sendall(data)
+
+def run():
+    port = -1
+    arg_idx = 1
+    verbosity = 3
+    host = ""
+    while (arg_idx < len(sys.argv)):
+        current_arg = sys.argv[arg_idx]
+        if (current_arg[0] == '-'):
+            if (current_arg == "-v"):
+                if (arg_idx + 1 >= len(sys.argv)):
+                    usage("Missing value for verbosity")
+                verbosity = int(sys.argv[arg_idx + 1])
+                arg_idx += 1
+            else:
+                usage()
+        elif (len(host) == 0):
+            host = current_arg
+        elif (port == -1):
+            port = int(current_arg)
+            if (port < 0):
+                usage("Unexpected negative value for port: " + current_arg)
+            else:
+                usage("Unexpected additional argument: " + current_arg)
+        arg_idx += 1
+
+    if (port == -1):
+        usage("Missing arguments")
+
+    
