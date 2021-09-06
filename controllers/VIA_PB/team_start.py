1187e97791ebcce07e8dbd1b67125f92f707cb34
babaev1 <57300002+babaev1@users.noreply.github.com>
2021-09-06 09:54:15 +0200

    поправил imu activation

--- controllers/VIA_PB/team_start.py
+++ controllers/VIA_PB/team_start.py
@@ -1,18 +1,23 @@
+import datetime
+import os
+import subprocess
+import win32gui, win32con
 import time
-from threading import Thread
-from communication_manager import CommunicationManager
-#import main_pb
+import math
 
-def process_11():
-    execfile("main_pb_11.py")
+t = win32gui.GetForegroundWindow()
+win32gui.ShowWindow(t, win32con.SW_MINIMIZE)
 
-def process_12():
-    execfile("main_pb_12.py")
+with open('output10001.txt', "a") as f1001:
+    print(datetime.datetime.now(), file = f1001)
+    subprocess.Popen(['python', 'main_pb.py', '10001'], stderr=f1001)
+with open('output10002.txt', "a") as f1002:
+    print(datetime.datetime.now(), file = f1002)
+    subprocess.Popen(['python', 'main_pb.py', '10002'], stderr=f1002)
+with open('output10021.txt', "a") as f1021:
+    print(datetime.datetime.now(), file = f1021)
+    subprocess.Popen(['python', 'main_pb.py', '10021'], stderr=f1021)
+with open('output10022.txt', "a") as f1022:
+    print(datetime.datetime.now(), file = f1022)
+    subprocess.Popen(['python', 'main_pb.py', '10022'], stderr=f1022)
 
-t11 = Thread(target = process_11, daemon = True)
-t11.start()
-t11.join()
-
-t12 = Thread(target = process_12, daemon = True)
-t12.start()
-t12.join()
