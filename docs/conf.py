cb86a10a66df72f54df3af4425f6d14e89b24bb9
ar0usel <khohklov.iyu@gmail.com>
2021-09-08 12:51:15 +0300

    fixing path to rst

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-master_doc = 'source/modules'
+master_doc = 'source/index'
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
