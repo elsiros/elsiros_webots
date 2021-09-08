8a3e6924e7e53f86297d52906794aa9373750a66
ar0usel <khohklov.iyu@gmail.com>
2021-09-08 12:42:30 +0300

    changing index to main

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-master_doc = 'index'
+master_doc = 'modules'
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
