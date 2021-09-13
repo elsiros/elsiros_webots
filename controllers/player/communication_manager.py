c278b0db8ced5f399448a159d064d3db4d20ac88
babaev1 <57300002+babaev1@users.noreply.github.com>
2021-09-13 16:35:59 +0300

    Squashed commit of the following:
    commit e672f715542f63e0324a1cb8323beedce77be7ae
    Author: Egor@T480s <egordv@gmail.com>
    Date:   Mon Sep 13 16:09:18 2021 +0300
    
        player.exe recompiled
    
    commit 79ac917356629745d53915f7ff16d361c1b467d2
    Merge: 94767ad 0f2e512
    Author: Ivan Khokhlov <khokhlov.iyu@gmail.com>
    Date:   Mon Sep 13 16:02:03 2021 +0300
    
        Merge pull request #47 from RoboCupHSJL/fix/player_controller
    
        Fix/player controller
    
    commit 0f2e5123169421c3723a1d1f20b56bda34556272
    Merge: a11fa97 94767ad
    Author: Ivan Khokhlov <khokhlov.iyu@gmail.com>
    Date:   Mon Sep 13 16:01:53 2021 +0300
    
        Merge branch 'release/player_controller' into fix/player_controller
    
    commit a11fa97a1f9311fc0827a6240f590dc906d02d27
    Author: ar0usel <khohklov.iyu@gmail.com>
    Date:   Mon Sep 13 15:57:41 2021 +0300
    
        fixed protoNames
    
    commit c2d7f6fdb25f20011481572af44bb13c84b1cdbd
    Author: ar0usel <khohklov.iyu@gmail.com>
    Date:   Mon Sep 13 15:56:52 2021 +0300
    
        added gps blurring and consistency
    
    commit 853ddee7f1f38dce41b1b9e3d0b013798fc2458d
    Author: ar0usel <khohklov.iyu@gmail.com>
    Date:   Mon Sep 13 15:06:07 2021 +0300
    
        fixed visible area calculation

---
+++ controllers/player/communication_manager.py
@@ -0,0 +1,116 @@
+"""[summary]
+Returns:
+[type]: [description]
+"""
+import queue
+import time
+import logging
+from threading import Thread
+
+from robot_client import RobotClient
+
+
+class CommunicationManager():
+    """[summary]
+    """
+    def __init__(self, maxsize=1, host='127.0.0.1', port=10001):
+        verbosity = 4
+        self.client = RobotClient(host, port, verbosity)
+        self.client.connect_client()
+        self.maxsize = maxsize
+        self.messages = queue.Queue(maxsize)
+        self.sensors = {}
+
+    def enable_sensors(self, sensors) -> None:
+        for sensor in sensors:
+            self.client.add_initial_sensor(sensor, sensors[sensor])
+            if sensor == "recognition":
+                self.sensors.update({"BALL": queue.Queue(self.maxsize)})
+                self.sensors.update({"RED_PLAYER_1": queue.Queue(self.maxsize)})
+                self.sensors.update({"RED_PLAYER_2": queue.Queue(self.maxsize)})
+                self.sensors.update({"BLUE_PLAYER_1": queue.Queue(self.maxsize)})
+                self.sensors.update({"BLUE_PLAYER_2": queue.Queue(self.maxsize)})
+
+            self.sensors.update({str(sensor): queue.Queue(self.maxsize)})
+        self.sensors.update({"time": queue.Queue(1)})
+        self.client.send_request("init")
+
+    def get_sensor(self, name) -> dict:
+        """[summary]
+
+        Args:
+            name ([type]): [description]
+
+        Returns:
+            dict: [description]
+        """
+        value_dict = {}
+        if not name in self.sensors:
+            logging.error("sensor is not enable")
+            #return "sensor is not enable"
+        elif not self.sensors[name].empty():
+            value_dict = self.sensors[name].get()
+            #logging.warn("nothing in queue")
+            #return False
+        #else:
+            #return self.sensors[name].get()
+        return value_dict
+
+    def add_to_queue(self, message):
+        if self.messages.full():
+            self.messages.get()
+            self.messages.put(message)
+        else:
+            self.messages.put(message)
+
+    def send_message(self):
+        while(not self.messages.empty()):
+            self.client.send_request("positions", self.messages.get())
+
+    def update_history(self, message):
+        for sensor in message:
+            if self.sensors[sensor].full():
+                self.sensors[sensor].get()
+                self.sensors[sensor].put(message[sensor])
+            else:
+                self.sensors[sensor].put(message[sensor])
+
+    def run(self):
+        data = {"head_pitch": 0.0, "head_yaw": 0.9}
+
+        self.add_to_queue(data)
+        while(True):
+            self.send_message()
+            message = self.client.receive()
+            # print(message)
+            self.update_history(message)
+
+    def test_run(self):
+        # пример отправки данных серв
+        self.WBservosList = ["right_ankle_roll", "right_ankle_pitch", "right_knee", "right_hip_pitch",
+                             "right_hip_roll", "right_hip_yaw", "right_elbow_pitch", "right_shoulder_twirl",
+                             "right_shoulder_roll", "right_shoulder_pitch", "pelvis_yaw", "left_ankle_roll",
+                             "left_ankle_pitch", "left_knee", "left_hip_pitch", "left_hip_roll", "left_hip_yaw",
+                             "left_elbow_pitch", "left_shoulder_twirl", "left_shoulder_roll",
+                             "left_shoulder_pitch", "head_yaw", "head_pitch"]
+        while(True):
+            time.sleep(0.02)
+            # пример получения данных из включенного и существующего сенсора
+            print("ball: ", self.get_sensor("BALL"))
+            print("gps_body: ", self.get_sensor("gps_body"))
+
+
+if __name__ == '__main__':
+    manager = CommunicationManager(1, '127.0.0.1', 10001)
+    # инициализация сенсоров
+    sensors = {"left_knee_sensor": 5, "right_knee_sensor": 5, "left_ankle_pitch_sensor": 5, "right_ankle_pitch_sensor": 5, "right_hip_pitch_sensor": 5, "left_hip_pitch_sensor": 5,  "gps_body": 5,"head_pitch_sensor": 5, "head_yaw_sensor": 5, "imu_body": 5, "recognition": 5}#
+    # sensors = {"gps_body": 5, "imu_head": 5, "imu_body": 5,  "camera": 20}#
+    manager.enable_sensors(sensors)
+
+    th1 = Thread(target=manager.run)
+    th2 = Thread(target=manager.test_run)
+    #manager.run()
+    th1.start()
+    th2.start()
+    th1.join
+    th2.join
