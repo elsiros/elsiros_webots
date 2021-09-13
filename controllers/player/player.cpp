94767ad98e661701926c4ae6efa13a6eafd991b9
Egor@T480s <egordv@gmail.com>
2021-09-13 07:28:24 +0300

    Player.exe modified back for WIN32 compilation, player.exe recompiled

--- controllers/player/player.cpp
+++ controllers/player/player.cpp
@@ -15,21 +15,77 @@
 #include <assert.h>
 #include <math.h>
 #include <stdio.h>
-
-#include <sys/time.h>
 #include <fstream>
+#ifndef _WIN32
+#include <sys/time.h>
+#endif
+#include <time.h>
+#include <windows.h> 
+
+#ifdef _WIN32
+const __int64 DELTA_EPOCH_IN_MICROSECS = 11644473600000000;
+
+/* IN UNIX the use of the timezone struct is obsolete;
+ I don't know why you use it. See http://linux.about.com/od/commands/l/blcmdl2_gettime.htm
+ But if you want to use this structure to know about GMT(UTC) diffrence from your local time
+ it will be next: tz_minuteswest is the real diffrence in minutes from GMT(UTC) and a tz_dsttime is a flag
+ indicates whether daylight is now in use
+*/
+struct timezone2
+{
+    __int32  tz_minuteswest; /* minutes W of Greenwich */
+    bool  tz_dsttime;     /* type of dst correction */
+};
+
+struct timeval2 {
+    __int32    tv_sec;         /* seconds */
+    __int32    tv_usec;        /* microseconds */
+};
+
+int gettimeofday(struct timeval2* tv/*in*/, struct timezone2* tz/*in*/)
+{
+    FILETIME ft;
+    __int64 tmpres = 0;
+    TIME_ZONE_INFORMATION tz_winapi;
+    int rez = 0;
+
+    ZeroMemory(&ft, sizeof(ft));
+    ZeroMemory(&tz_winapi, sizeof(tz_winapi));
+
+    GetSystemTimeAsFileTime(&ft);
+
+    tmpres = ft.dwHighDateTime;
+    tmpres <<= 32;
+    tmpres |= ft.dwLowDateTime;
+
+    /*converting file time to unix epoch*/
+    tmpres /= 10;  /*convert into microseconds*/
+    tmpres -= DELTA_EPOCH_IN_MICROSECS;
+    tv->tv_sec = (__int32)(tmpres * 0.000001);
+    tv->tv_usec = (tmpres % 1000000);
+
+
+    //_tzset(),don't work properly, so we use GetTimeZoneInformation
+    rez = GetTimeZoneInformation(&tz_winapi);
+    tz->tz_dsttime = (rez == 2) ? true : false;
+    tz->tz_minuteswest = tz_winapi.Bias + ((rez == 2) ? tz_winapi.DaylightBias : 0);
+
+    return 0;
+}
+#endif
+
 #ifdef _WIN32
 #include <winsock.h>
 typedef int socklen_t;
 #define MSG_NOSIGNAL 0
 void usleep(__int64 usec) {
-  HANDLE timer;
-  LARGE_INTEGER ft;
-  ft.QuadPart = -10 * usec;
-  timer = CreateWaitableTimer(NULL, TRUE, NULL);
-  SetWaitableTimer(timer, &ft, 0, NULL, NULL, 0);
-  WaitForSingleObject(timer, INFINITE);
-  CloseHandle(timer);
+    HANDLE timer;
+    LARGE_INTEGER ft;
+    ft.QuadPart = -10 * usec;
+    timer = CreateWaitableTimer(NULL, TRUE, NULL);
+    SetWaitableTimer(timer, &ft, 0, NULL, NULL, 0);
+    WaitForSingleObject(timer, INFINITE);
+    CloseHandle(timer);
 }
 #else
 
