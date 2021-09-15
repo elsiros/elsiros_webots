fa20cf484ae63dd162fd8ae73bddf4a0bf28b5b0
babaev1 <57300002+babaev1@users.noreply.github.com>
2021-09-15 18:41:33 +0300

    renaming controller

--- controllers/referee/модуль1.py
+++
@@ -1,9 +0,0 @@
-import os
-import signal
-import subprocess
-
-external_controllers_process = subprocess.Popen(['python', 'start_teams.py'])
-
-input('press Enter')
-subprocess.Popen("TASKKILL /F /PID {pid} /T".format(pid=external_controllers_process.pid))
-#external_controllers_process.kill()
